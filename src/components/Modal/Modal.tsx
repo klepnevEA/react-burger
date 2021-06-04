d80052fd0687253a9a49f55020d43765612f39d8
klepnev-ea <klepnev-ea>
2021-06-04 15:25:40 +0300

    add portal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./index.module.css";
+import { createPortal } from "react-dom";
+
+const reactModals = document.getElementById("react-modals");
 
 function Modal(props: any) {
   const { closeModal } = props;
@@ -14,16 +17,21 @@ function Modal(props: any) {
     });
   });
 
-  return (
-    <div className={styles.modal}>
-      <div className={`p-10 pn-15 ${styles["modal-body"]}`}>
-        <button className={styles["modal-close"]} onClick={closeModal}>
-          <CloseIcon type="primary" />
-        </button>
-        <div className={styles["modal-content"]}>{props.children}</div>
-      </div>
-    </div>
-  );
+  return reactModals
+    ? createPortal(
+        <>
+          <div className={styles.modal}>
+            <div className={`p-10 pn-15 ${styles["modal-body"]}`}>
+              <button className={styles["modal-close"]} onClick={closeModal}>
+                <CloseIcon type="primary" />
+              </button>
+              <div className={styles["modal-content"]}>{props.children}</div>
+            </div>
+          </div>
+        </>,
+        reactModals
+      )
+    : null;
 }
 
 export default Modal;
