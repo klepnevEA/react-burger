c4943e1decf68db98db0714c3a542f3220ed7270
klepnev-ea <klepnev-ea>
2021-08-12 21:51:10 +0300

    total price

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -12,28 +12,25 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   INGREDIENT_CONSTRUCTOR_ADD,
   INGREDIENT_CONSTRUCTOR_ADD__BUN,
+  INGREDIENT_CONSTRUCTOR_CLEAR,
   ORDER_DATAILS_OPEN,
 } from "../../services/actions";
 import { TDataItem } from "../../interface";
 import { useDrop } from "react-dnd";
 
 const BurgerConstructor = () => {
-  // const ingredientsBun = [];
-  // const [elements, setElements] = React.useState([]);
-  // const [draggedElements, setDraggedElements] = React.useState([]);
   const dispatch = useDispatch();
-  const { ingredientsConstructor, ingredientsConstructorBun } = useSelector(
-    (state: RootState) => state.ingredientConstructorBurger
-  );
+  const {
+    ingredientsConstructor,
+    ingredientsConstructorBun,
+    totalPriceBun,
+    totalPriceIngredients,
+  } = useSelector((state: RootState) => state.ingredientConstructorBurger);
   const { ingredients } = useSelector(
     (state: RootState) => state.ingredientList
   );
 
   const handleDrop = (itemId: any) => {
-    // let a = ingredients.filter(
-    //   (element: any) => element._id === itemId.ingredientId
-    // );
-    // console.log(a[0], ingredientsConstructor);
     dispatch({
       type: INGREDIENT_CONSTRUCTOR_ADD,
       ellement: ingredients.filter(
@@ -76,34 +73,23 @@ const BurgerConstructor = () => {
     },
   });
 
-  const [, dropTargetBunBottom] = useDrop({
-    accept: ["bun"],
-    drop(itemId) {
-      handleDropBun(itemId);
-    },
-  });
-
   const border = isHover ? "1px solid #2f2f37" : "1px solid transparent";
 
   const openOrder = () => {
-    dispatch({
-      type: ORDER_DATAILS_OPEN,
-    });
+    if (ingredientsConstructorBun.name && ingredientsConstructor.length) {
+      dispatch({
+        type: ORDER_DATAILS_OPEN,
+      });
+      dispatch({
+        type: INGREDIENT_CONSTRUCTOR_CLEAR,
+      });
+    }
   };
-
-  // for (let i = 0; i < ingredientsConstructor.length; i++) {
-  //   if (ingredientsConstructor[i]?.type !== "bun") {
-  //     ingredientsBun.push(ingredientsConstructor[i]);
-  //   }
-  // }
 
   return (
     <div className="pt-25">
-      <div className={`mb-10 ${styles.ingrediants}`}>
-        <div
-          className={`pl-8 mb-2 ${styles["ingredients-top"]}`}
-          ref={dropTargetBunTop}
-        >
+      <div className={`mb-10 ${styles.ingrediants}`} ref={dropTargetBunTop}>
+        <div className={`pl-8 mb-2 ${styles["ingredients-top"]}`}>
           {ingredientsConstructorBun.name && (
             <ConstructorElement
               type="top"
@@ -126,9 +112,9 @@ const BurgerConstructor = () => {
               }}
               ref={dropTarget}
             >
-              {ingredientsConstructor.map((item: TDataItem) => {
+              {ingredientsConstructor.map((item: TDataItem, index) => {
                 return (
-                  <li key={item._id} className={styles["ingredients-item"]}>
+                  <li key={index} className={styles["ingredients-item"]}>
                     <div className={`mr-1 ${styles["ingredients-drag"]}`}>
                       <DragIcon type="primary" />
                     </div>
@@ -143,10 +129,7 @@ const BurgerConstructor = () => {
             </ul>
           </div>
         </div>
-        <div
-          className={`pl-8 mt-2 ${styles["ingredients-bottom"]}`}
-          ref={dropTargetBunBottom}
-        >
+        <div className={`pl-8 mt-2 ${styles["ingredients-bottom"]}`}>
           {ingredientsConstructorBun.name && (
             <ConstructorElement
               type="bottom"
@@ -160,7 +143,9 @@ const BurgerConstructor = () => {
       </div>
 
       <div className={`pb-8 ${styles.order}`}>
-        <span className="text text_type_digits-medium">600</span>
+        <span className="text text_type_digits-medium">
+          {totalPriceBun + totalPriceIngredients}
+        </span>
         <CurrencyIcon type="primary" />
         <div className="pl-10">
           <Button type="primary" size="large" onClick={openOrder}>
--- src/services/reducers/ingredientBurger.tsx
+++ src/services/reducers/ingredientBurger.tsx
@@ -3,11 +3,14 @@
 import {
   INGREDIENT_CONSTRUCTOR_ADD,
   INGREDIENT_CONSTRUCTOR_ADD__BUN,
+  INGREDIENT_CONSTRUCTOR_CLEAR,
 } from "../actions";
 
 const ingredientConstructorBurgerReducerState = {
   ingredientsConstructor: [],
   ingredientsConstructorBun: {},
+  totalPriceBun: 0,
+  totalPriceIngredients: 0,
 };
 
 export const ingredientConstructorBurgerReducer = (
@@ -22,12 +25,24 @@ export const ingredientConstructorBurgerReducer = (
           ...state.ingredientsConstructor,
           action.ellement[0],
         ],
+        totalPriceIngredients:
+          state.totalPriceIngredients + action.ellement[0].price,
       };
     }
     case INGREDIENT_CONSTRUCTOR_ADD__BUN: {
       return {
         ...state,
         ingredientsConstructorBun: action.ellement[0],
+        totalPriceBun: action.ellement[0].price * 2,
+      };
+    }
+    case INGREDIENT_CONSTRUCTOR_CLEAR: {
+      return {
+        ...state,
+        ingredientsConstructorBun: [],
+        ingredientsConstructor: [],
+        totalPriceIngredients: 0,
+        totalPriceBun: 0,
       };
     }
     default: {
